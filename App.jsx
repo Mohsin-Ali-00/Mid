6e4b7de7fc24a13011db5a3ab8e3e493a62c71ef
Mohsin Ali <mohsin.soomro2000@gmail.com>
2025-03-25 10:19:49 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is my react app </p>
+    </div>
+  );
+};
+export default App;
